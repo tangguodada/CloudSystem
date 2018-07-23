ae6e4ec1b71db6db6b69f2c9081e9c5e6452ff55
yue-zhou <angle5503@126.com>
2018-07-23 14:21:43 +0800

    登出提示

--- src/components/admin/adminheader.vue
+++ src/components/admin/adminheader.vue
@@ -45,7 +45,18 @@
           this.$router.push('/adminmessage');
         },
         logout(){
-          this.$router.push('/login');
+          this.$confirm('此操作将退出改系统，是否继续?','提示',{
+            confirmButtonText:'确定',
+            cancleButtonText:'取消',
+            type:'warning'
+          }).then(() => {
+            this.$router.push('/login');
+          }).catch(() => {
+            this.$message({
+              type:'info',
+              message:'取消退出'
+            });
+          })
         }
 
       },
